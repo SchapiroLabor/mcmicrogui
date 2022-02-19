ea1a391c700daed6be341edd98fdc04b2a6064b3
semantic-release <semantic-release>
2022-02-19 18:59:23 +0100

    0.1.0
    Automatically generated by python-semantic-release

---
+++ setup.py
@@ -0,0 +1,11 @@
+#currently only used to configure automatic changelog/version numbering
+
+from setuptools import setup
+
+__version__ = "0.1.0"
+
+setup(
+   name="mcmicrogui",
+   version=__version__,
+ 
+)
